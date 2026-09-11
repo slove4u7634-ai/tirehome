eab14a3fe73b2d4bbf725b7bf1d127a73dcb8137
slove4u7634-ai <slove4u7634@gmail.com>
2026-09-11 12:03:33 +0900

    style: Shrink main.jpg size to 70% while maintaining aspect ratio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default async function Home() {
           <SearchForm />
         </section>
 
-        <div className="w-full mt-6 md:mt-8">
+        <div className="w-full md:w-[70%] mx-auto mt-6 md:mt-8">
           <img src="/img/main.jpg" alt="방문 및 장착방법 안내" className="w-full h-auto rounded-xl shadow-md object-contain" />
         </div>
       </div>
